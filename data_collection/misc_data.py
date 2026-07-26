ab408221d2c65b64f8a253f3bb2e549277195366
Daniel Beckley <dmbeckle@ucsc.edu>
2026-07-25 23:27:45 -0700

    updated contract merge

--- data_collection/misc_data.py
+++ data_collection/misc_data.py
@@ -15,13 +15,24 @@ draft = nfl.load_draft_picks().to_pandas()
 print(f"Draft Picks shape: {draft.shape}")
 print(draft.head())
 
-ccontracts = contracts.drop(columns=contract_col_drop).copy()
+ccontracts = contracts.drop(columns=contract_col_drop)
 ccontracts = ccontracts[ccontracts['position'].isin(pos_keep)]
 ccontracts = ccontracts[ccontracts['year_signed'] > 0]
 ccontracts = ccontracts[ccontracts['years'] > 0]
 ccontracts["pct_gtd_sign"] = ccontracts["guaranteed"] / ccontracts["value"]
 
-draft_picks = draft[draft['gsis_id'].isin(ccontracts['gsis_id'])].copy()
+draft_picks = draft[draft['gsis_id'].isin(ccontracts['gsis_id'])]
 draft_picks = draft_picks[draft_picks['gsis_id', 'round', 'pick']].drop_duplicates(subset=['gsis_id'], keep='first')
 
 ccontracts = ccontracts.merge (draft_picks, on='gsis_id', how='left')
+ccontracts[season] = ccontracts.apply(
+    lambda r: list(range(int(r["year_signed"]), int(r["year_signed"]) + int(r["years"]))), axis=1)
+
+ccontracts = ccontracts.explode("season").reset_index(drop=True)
+ccontracts = (ccontracts.sort_values("year_signed", ascending=False)
+             .drop_duplicates(subset=["gsis_id", "season"], keep="first"))
+
+ccontracts = ccontracts[ccontracts['season'] >= 2018].copy()
+
+print(f"\nmisc_data: {ccontracts.shape}")
+print(ccontracts.columns.tolist())
