4e172c2d1e28e8382a70bf78cea98e26b734b79f
Daniel Beckley <dmbeckle@ucsc.edu>
2026-09-11 17:58:27 -0700

    initial WR_TE_master

--- data_collection/WR_TE/WR_TE_master.py
+++ data_collection/WR_TE/WR_TE_master.py
@@ -25,10 +25,23 @@ WR_injuries = pd.read_excel(base_path / "WR_injuries.xlsx")
 
 espn = pd.read_excel(base_path / "WR_TE_ESPN.xlsx")
 espn = espn.drop(columns=['Targets', 'Yds'])
+WR_espn = espn[espn['Pos'] == 'WR']
+TE_espn = espn[espn['Pos'] == 'TE']
 
 nextgen = pd.read_excel(base_path / "WR_TE_nextgen.xlsx")
 nextgen = nextgen.drop(columns='TD')
+WR_nextgen = nextgen[nextgen['POS'] == 'WR']
+TE_nextgen = nextgen[nextgen['POS'] == 'TE']
 
 misc = pd.read_csv(data_path / "misc_data.csv")
 WR_misc = misc[misc['position'] == 'WR']
 TE_misc = misc[misc['position'] == 'TE']
+
+# renames
+renames = {'Deonte Harris': 'Deonte Harty', 'Josh Palmer': 'Joshua Palmer', 'Scott Miller': 'Scotty Miller', 'Will Fuller': 'William Fuller'}
+
+for df in [WR_base, WR_TD, WR_shares, WR_redzone, WR_injuries, WR_espn, TE_espn, WR_misc, TE_misc]:
+    df['Player'] = df['Player'].str.strip()
+    df['Player'] = df['Player'].replace(renames)
+    if 'TM' in df.columns:
+        df['TM'] = df['TM'].str.strip()
