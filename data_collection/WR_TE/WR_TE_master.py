2bca64235e3b1d6aa54df7bc7aa38547291901be
Daniel Beckley <dmbeckle@ucsc.edu>
2026-09-14 14:31:00 -0700

    initial master sets generated

--- data_collection/WR_TE/WR_TE_master.py
+++ data_collection/WR_TE/WR_TE_master.py
@@ -8,30 +8,41 @@ sys.path.append(str(data_path))
 from utils import vol_check, apply_vol_check, normalize_player
 
 ## prep and merge data
-# read data and drop/fix repetitive columns
-WR_base = pd.read_excel(base_path / "WR_Base.xlsx")
+# read data and drop/fix necessary columns
+WR_base = pd.read_excel(base_path / "WR_base.xlsx")
 WR_base = WR_base.drop(columns=['Player (TM)'])
+TE_base = pd.read_excel(base_path / "TE_base.xlsx")
+TE_base = TE_base.drop(columns=['Player (TM)'])
 
-WR_TD = pd.read_excel(base_path / "WR_TD.xlsx")
-WR_TD = WR_TD.drop(columns=['Player (TM)', 'GAMES'])
-
-WR_shares = pd.read_excel(base_path / "WR_shares.xlsx")
-WR_shares = WR_shares.drop(columns=['GAMES'])
+WR_td = pd.read_excel(base_path / "WR_TD.xlsx")
+WR_td = WR_td.drop(columns=['Player (TM)'])
+TE_td = pd.read_excel(base_path / "TE_TD.xlsx")
+TE_td = TE_td.drop(columns=['Player (TM)'])
 
 WR_redzone = pd.read_excel(base_path / "WR_redzone.xlsx")
-WR_redzone = WR_redzone.drop(columns=['Player TM'])
+WR_redzone = WR_redzone.drop(columns=['Player (TM)', 'TM']) # TM current to date not respective to season
+TE_redzone = pd.read_excel(base_path / "TE_redzone.xlsx")
+TE_redzone = TE_redzone.drop(columns=['Player (TM)', 'TM'])
 
 WR_injuries = pd.read_excel(base_path / "WR_injuries.xlsx")
+TE_injuries = pd.read_excel(base_path / "TE_injuries.xlsx")
+
+WR_shares = pd.read_excel(base_path / "WR_shares.xlsx")
+TE_shares = pd.read_excel(base_path / "TE_shares.xlsx")
 
 espn = pd.read_excel(base_path / "WR_TE_ESPN.xlsx")
-espn = espn.drop(columns=['Targets', 'Yds'])
 WR_espn = espn[espn['Pos'] == 'WR']
 TE_espn = espn[espn['Pos'] == 'TE']
 
 nextgen = pd.read_excel(base_path / "WR_TE_nextgen.xlsx")
-nextgen = nextgen.drop(columns='TD')
 WR_nextgen = nextgen[nextgen['POS'] == 'WR']
 TE_nextgen = nextgen[nextgen['POS'] == 'TE']
+
+WR_adv = pd.read_excel(base_path / 'WR_adv.xlsx')
+WR_adv = WR_adv.drop(columns={'Player (TM)'})
+TE_adv = pd.read_excel(base_path / 'TE_adv.xlsx')
+TE_adv = TE_adv.drop(columns={'Player (TM)'})
+
 
 misc = pd.read_csv(data_path / "misc_data.csv")
 WR_misc = misc[misc['position'] == 'WR']
@@ -40,8 +51,43 @@ TE_misc = misc[misc['position'] == 'TE']
 # renames
 renames = {'Deonte Harris': 'Deonte Harty', 'Josh Palmer': 'Joshua Palmer', 'Scott Miller': 'Scotty Miller', 'Will Fuller': 'William Fuller'}
 
-for df in [WR_base, WR_TD, WR_shares, WR_redzone, WR_injuries, WR_espn, TE_espn, WR_misc, TE_misc]:
+for df in [WR_base, TE_base, WR_td, TE_td, WR_redzone, TE_redzone, WR_injuries, TE_injuries, WR_shares, TE_shares,
+        WR_espn, TE_espn, WR_nextgen, TE_nextgen, WR_misc, TE_misc]:
     df['Player'] = df['Player'].str.strip()
     df['Player'] = df['Player'].replace(renames)
+    df['Player'] = df['Player'].apply(normalize_player)
     if 'TM' in df.columns:
         df['TM'] = df['TM'].str.strip()
+
+merge_keys = ['Player', 'TM', 'Year']
+backup_merge = ['Player', 'Year']
+WR_master = WR_base.merge(WR_td, on=merge_keys, how="outer")
+WR_master = WR_master.merge(WR_redzone, on=backup_merge, how="outer")
+WR_master = WR_master.merge(WR_injuries, on=backup_merge, how="outer")
+WR_master = WR_master.merge(WR_shares, on=merge_keys, how="outer")
+WR_master = WR_master.merge(WR_espn, on=merge_keys, how="outer")
+WR_master = WR_master.merge(WR_nextgen, on=merge_keys, how="outer")
+WR_master = WR_master.merge(WR_misc, on=backup_merge, how="outer")
+
+TE_master = TE_base.merge(TE_td, on=merge_keys, how="outer")
+TE_master = TE_master.merge(TE_redzone, on=backup_merge, how="outer")
+TE_master = TE_master.merge(TE_injuries, on=backup_merge, how="outer")
+TE_master = TE_master.merge(TE_shares, on=merge_keys, how="outer")
+TE_master = TE_master.merge(TE_espn, on=merge_keys, how="outer")
+TE_master = TE_master.merge(TE_nextgen, on=merge_keys, how="outer")
+TE_master = TE_master.merge(TE_misc, on=backup_merge, how="outer")
+
+
+## data cleaning
+WR_tailoff_df, WR_insuff_df = vol_check(WR_master, vol_cols=["TGT"], prod_thresholds=[40])
+WR_master, WR_insuff_dropped, WR_tailoff_dropped = apply_vol_check(WR_master, WR_tailoff_df, WR_insuff_df)
+WR_insuff_dropped.to_csv(base_path / "WR_dropped_insuff.csv", index=False)
+WR_tailoff_dropped.to_csv(base_path / "WR_dropped_tailoff.csv", index=False)
+
+TE_tailoff_df, TE_insuff_df = vol_check(TE_master, vol_cols=["TGT"], prod_thresholds=[30])
+TE_master, TE_insuff_dropped, TE_tailoff_dropped = apply_vol_check(TE_master, TE_tailoff_df, TE_insuff_df)
+TE_insuff_dropped.to_csv(base_path / "TE_dropped_insuff.csv", index=False)
+TE_tailoff_dropped.to_csv(base_path / "TE_dropped_tailoff.csv", index=False)
+
+WR_master.to_csv(base_path / "WR_MASTER.csv", index=False)
+TE_master.to_csv(base_path / "TE_MASTER.csv", index=False)
