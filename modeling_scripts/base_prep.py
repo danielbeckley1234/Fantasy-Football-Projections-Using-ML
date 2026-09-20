dfda7ea27ca495587bc311fc973d9ce2abe868ae
Daniel Beckley <dmbeckle@ucsc.edu>
2026-09-20 00:04:46 -0700

    rewrote to be applicable to any position

--- modeling_scripts/base_prep.py
+++ modeling_scripts/base_prep.py
@@ -1,3 +1,4 @@
+# base_prep.py: feature engineering, lagged features
 import pandas as pd
 import numpy as np
 
@@ -20,59 +21,16 @@ def build_features(
     # lag respective statistics
     g = df.groupby('gsis_id', group_keys=False)
     for col in lag1s:
-        df[f'{col}/G_lag1'] = g[f'{col}/G'].shift(1) 
-
+        df[f'{col}_lag1'] = g[f'{col}'].shift(1) 
     for col in lag2s:
-        df[f'{col}/G_lag1'] = g[f'{col}/G'].shift(1)
-        df[f'{col}/G_lag2'] = g[f'{col}/G'].shift(2)
-
+        df[f'{col}_lag1'] = g[f'{col}'].shift(1)
+        df[f'{col}_lag2'] = g[f'{col}'].shift(2)
     for col in targets:
         df[f'{col}/G_lag1'] = g[f'{col}/G'].shift(1)
         df[f'{col}/G_lag2'] = g[f'{col}/G'].shift(2)
-        df[f'{col}/G_lag3'] = g[f'{col}/G'].shift(3)   
+        df[f'{col}/G_lag3'] = g[f'{col}/G'].shift(3) 
 
-# stage/trajectory/health in career evaluator
-    def pct_of_peak_role(sub: pd.DataFrame, pos: str) -> pd.DataFrame:
-        sub = sub.sort_values('Year')
-        if pos == 'RB':
-            prior_att, prior_tgt = [], []
-            pct_peak_att, pct_peak_tgt = [], []
-        for _, row in sub.iterrows():
-            att_val = row['ATT'] if pd.notna(row['ATT']) else 0.0
-            tgt_val = row['TGT'] if pd.notna(row['TGT']) else 0.0
-
-            # rookies/first observed season
-            if len(prior_att) == 0:
-                pa, pt = np.nan, np.nan
-            else:
-                peak_att = max(prior_att)
-                peak_tgt = max(prior_tgt)
-                pa = att_val / peak_att if peak_att > 0 else np.nan
-                pt = tgt_val / peak_tgt if peak_tgt > 0 else np.nan
-            pct_peak_att.append(pa)
-            pct_peak_tgt.append(pt)
-            prior_att.append(att_val)
-            prior_tgt.append(tgt_val)
-        return pd.DataFrame({'pct_peak_att': pct_peak_att, 'pct_peak_tgt': pct_peak_tgt}, index=sub.index)
-
-    peak_df = g.apply(pct_of_peak_role)
-    if isinstance(peak_df.index, pd.MultiIndex):
-        peak_df = peak_df.reset_index(level=0, drop=True)
-    df[['pct_peak_att', 'pct_peak_tgt']] = peak_df
-    g = df.groupby('gsis_id', group_keys=False)
-
-    # specific position work
-    if pos == 'RB':
-        for col in ['ATT', 'TGT']:
-                df[f'{col}/G_std3'] = g[f'{col}/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
-    elif pos.isin['WR', 'TE']:
-        df[f'TGT/G_std3'] = g[f'TGT/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
-    # elif pos == 'QB':
-    #     df[f'PATT/G_std3'] = g[f'PATT/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
-    # elif pos == 'K':
-    #     df[f'FgATT/G_std3'] = g[f'FgATT/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
-
-    # universal feature engineered columns
+        # universal feature engineered columns
     df['years_exp'] = df['Year'] - df['rookie_season']
     df['team_change'] = (df['TM'] != g['TM'].shift(1)).astype(int)
     df.loc[g.cumcount() == 0, 'team_change'] = 0
@@ -81,6 +39,46 @@ def build_features(
     df['draft_round_filled'] = df['draft_round'].fillna(8)
     df['draft_pick_filled'] = df['draft_pick'].fillna(300)
     df['age_sq'] = df['age'] ** 2
+
+# stage/trajectory/health in career evaluator
+    def pct_of_peak_role(sub: pd.DataFrame) -> pd.DataFrame:
+        sub = sub.sort_values('Year')
+        prior_vol, pct_peak_vol = [], []
+        for _, row in sub.iterrows():
+            if 'touches' in df.columns:
+                vol_val = row['touches'] if pd.notna(row['touches']) else 0.0
+            # elif 'pATT' in df.columns():
+            #     vol_val = row['pATT'] if pd.notna(row['pATT']) else 0.0
+            # elif 'FG+XP' in df.columns():
+            #     vol_val = row['FG+XP'] if pd.notna(row['FG+XP']) else 0.0
+
+            # rookies/first observed season
+            if len(prior_vol) == 0:
+                pv = np.nan
+            else:
+                peak_vol = max(prior_vol)
+                pv = vol_val / peak_vol if peak_vol > 0 else np.nan
+            pct_peak_vol.append(pv)
+            prior_vol.append(vol_val)
+        return pd.DataFrame({'pct_peak_vol': pct_peak_vol}, index=sub.index)
+
+    peak_df = g.apply(pct_of_peak_role)
+    if isinstance(peak_df.index, pd.MultiIndex):
+        peak_df = peak_df.reset_index(level=0, drop=True)
+    df[['pct_peak_vol']] = peak_df
+    g = df.groupby('gsis_id', group_keys=False)
+
+    # positional volume stds
+    if pos == 'RB':
+        for col in ['ATT', 'TGT']:
+                df[f'{col}/G_std3'] = g[f'{col}/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
+    elif pos in ['WR', 'TE']:
+        df[f'TGT/G_std3'] = g[f'TGT/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
+    # elif pos == 'QB':
+    #     df[f'pATT/G_std3'] = g[f'pATT/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
+    # elif pos == 'K':
+    #     df[f'FGA/G_std3'] = g[f'FGA/G'].apply(lambda s: s.shift(1).rolling(3, min_periods=2).std())
+
 
     for col in targets:
         df[f'target_{col}/G'] = g[f'{col}/G'].shift(-1)
