cd409bd66249239026eccb036bd17887c5d60359
Daniel Beckley <dmbeckle@ucsc.edu>
2026-09-20 16:40:28 -0700

    fixing

--- modeling_scripts/RB_map.py
+++ modeling_scripts/RB_map.py
@@ -1,22 +1,19 @@
-import pandas as pd
-import numpy as np
-
 RB_targets = ['ATT', 'RusYDS', 'RusTD', 'TGT', 'REC', 'RecYDS', 'RecTD']
 RB_counting = RB_targets + ['Snaps', 'Routes', 'In5ATT', 'In5REC', 'In5TG']
-RB_lag1s = ['Snaps/G', 'significant_injury_lag1','RusY/A', 'RYOE/ATT', 'RYOE%', 'Rush%', 'Routes/G', 'Open',
-            'Catch', 'YAC', 'Overall', 'Tgt%', 'Y/RR', 'RusTD%', 'xRusTD%',]
-RB_lag2s = ['In5ATT', 'In5Rush%', 'In5REC/G', 'In5TG/G']
+RB_lag1s = ['Snaps/G', 'significant_injury','RusY/A', 'RYOE/ATT', 'RYOE%', 'Rush%', 'Routes/G', 'Open',
+            'Catch', 'YAC', 'Overall', 'Tgt%', 'Y/RR', 'RusTD%', 'xRusTD%', 'RecTD%', 'xRecTD%']
+RB_lag2s = ['In5ATT/G', 'In5Rush%', 'In5REC/G', 'In5TG/G']
 
 RB_cross_features = ['Snaps/G', 'Snaps/G_lag1', 'significant_injury_lag1', 
     'draft_round_filled', 'draft_pick_filled', 'age', 'age_sq', 
     'years', 'inflated_apy', 'inflated_guaranteed', 'pct_gtd_sign', 
-    'years_exp', 'years_since_full', 'team_change', 'games_missed_rate']
+    'years_exp', 'years_since_prod', 'years_since_full', 'team_change', 'games_missed_rate', 'pct_peak_vol']
 
 # for att/rusyds
 RB_rush_base_features = ['ATT/G', 'ATT/G_lag1', 'ATT/G_lag2', 'ATT/G_lag3', 'ATT/G_std3',
     'RusYDS/G', 'RusYDS/G_lag1', 'RusYDS/G_lag2', 'RusYDS/G_lag3',
     'RusY/A', 'RusY/A_lag1', 'TmRBWR', 'RYOE/ATT', 'RYOE/ATT_lag1',
-    'RYOE%', 'RYOE%_lag1', 'Rush%', 'Rush%_lag1', 'pct_peak_att']
+    'RYOE%', 'RYOE%_lag1', 'Rush%', 'Rush%_lag1']
 
 # for all receiving statistics
 RB_rec_base_features = ['TGT/G', 'TGT/G_lag1', 'TGT/G_lag2', 'TGT/G_lag3', 'TGT/G_std3',
@@ -24,7 +21,7 @@ RB_rec_base_features = ['TGT/G', 'TGT/G_lag1', 'TGT/G_lag2', 'TGT/G_lag3', 'TGT/
     'RecYDS/G', 'RecYDS/G_lag1', 'RecYDS/G_lag2', 'RecYDS/G_lag3',
     'Routes/G', 'Routes/G_lag1', 'Open', 'Open_lag1', 'Catch', 'Catch_lag1', 
     'YAC', 'YAC_lag1', 'Overall', 'Overall_lag1',
-    'Tgt%', 'Tgt%_lag1', 'pct_peak_tgt', 'Y/RR', 'Y/RR_lag1']
+    'Tgt%', 'Tgt%_lag1', 'Y/RR', 'Y/RR_lag1']
 
 RB_target_feature_map = {
     'ATT': RB_cross_features + RB_rush_base_features,
