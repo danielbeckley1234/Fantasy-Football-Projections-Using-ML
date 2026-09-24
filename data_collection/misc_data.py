4aac2e3cac056c22a3f3beb9ba254d928c582e92
Daniel Beckley <dmbeckle@ucsc.edu>
2026-09-24 10:51:11 -0700

    added 2026 wk 1 depth charts

--- data_collection/misc_data.py
+++ data_collection/misc_data.py
@@ -1,22 +1,19 @@
-# misc_data.py: assembles the miscellaneous data pulled from the nflreadpy library, including contract and draft data. 
+# misc_data.py: assembles the miscellaneous data pulled from the nflreadpy library, including contract, draft, and starter data. 
 import nflreadpy as nfl
 import pandas as pd
 
-
+## construct miscellaneous dataset
 # filters
 contract_keep = ['gsis_id', 'year_signed', 'years', 'inflated_value', 'inflated_guaranteed', 'inflated_apy']
-pos_keep = ['QB', 'RB', 'FB', 'WR', 'TE', 'K']
+pos_keep = ['QB', 'RB', 'WR', 'TE', 'K']
 player_keep = ['display_name', 'position', 'gsis_id', 'birth_date', 'draft_round', 'draft_pick', 'rookie_season', 'last_season']
 inc_years = list(range(2018, 2027))
 
 # load data
 players = nfl.load_players().to_pandas()
-print(f"Players shape: {players.shape}")
-print(players.head())
-
+print(players.head(5))
 contracts = nfl.load_contracts().to_pandas()
-print(f"Contracts shape: {contracts.shape}")
-print(contracts.head())
+print(contracts.head(5))
 
 # initial filtering and explode seasons for each year of interest
 players = players[player_keep]
@@ -51,5 +48,31 @@ misc = misc[misc['Year'] >= 2018]
 
 print(f"\nmisc data: {misc.shape}")
 print(misc.columns.tolist())
-
 misc.to_csv("misc_data.csv", index=False)
+
+
+## create list of projected starters to apply models to
+dcharts = nfl.load_depth_charts(seasons=[2026]).to_pandas()
+print(dcharts.head(5))
+dchart_keep = ['dt', 'team', 'player_name', 'gsis_id', 'pos_abb', 'pos_rank']
+dcharts = dcharts[dchart_keep]
+dcharts['dt'] = pd.to_datetime(dcharts['dt'])
+dcharts = dcharts.rename(columns={'player_name': 'Player', 'pos_abb': 'Pos', 'team': 'TM'})
+dcharts = dcharts[dcharts['Pos'].isin(pos_keep)]
+dcharts = dcharts[dcharts['dt'].dt.date == pd.Timestamp('2026-09-09').date()] # 2026 week 1 depth charts
+
+pos_rank_lims = {'QB': 1, 'RB': 2, 'WR': 4, 'TE': 2, 'K': 1}
+mask = pd.Series(False, index=dcharts.index)
+
+for pos, lim in pos_rank_lims.items():
+    mask |= (dcharts['Pos'] == pos) & (dcharts['pos_rank'] <= lim)
+
+dcharts = dcharts[mask]
+
+print(f"\nstarter data: {dcharts.shape}")
+print(dcharts.columns.tolist())
+dcharts.to_csv("starters.csv", index=False)
+
+
+
+
